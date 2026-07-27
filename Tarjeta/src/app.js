e06c81985cee8e29b08f0cf0c3ca7b0c273b9599
Pascual890 <pascualtobias@hotmail.com>
2026-07-27 18:44:29 -0500

    tarjeta

---
+++ Tarjeta/src/app.js
@@ -0,0 +1,9 @@
+const onxrloaded = () => {
+  XR8.XrController.configure({
+    imageTargetData: [
+      require('../image-targets/2.json')
+
+    ],
+  })
+}
+window.XR8 ? onxrloaded() : window.addEventListener('xrloaded', onxrloaded)

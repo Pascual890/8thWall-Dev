4132833da9a6c6416d1b891f9cdcec0edeece5e9
Pascual890 <pascualtobias@hotmail.com>
2026-08-05 09:20:21 -0500

    tarjeta

--- Tarjeta/src/boton-video.ts
+++ Tarjeta/src/boton-video.ts
@@ -3,29 +3,38 @@ import * as ecs from '@8thwall/ecs'
 ecs.registerComponent({
   name: 'boton-video',
   schema: {
-    reproductorVideo: ecs.eid, 
+    reproductorVideo: ecs.eid,
   },
-  add: (world, component) => {
-    const { reproductorVideo } = component.schema
-
-    // Esta es la función que se ejecutará al tocar
-    const alternarVideo = () => {
-      console.log("¡Botón presionado!") // Mensaje de prueba
-      
+  stateMachine: ({world, eid, schemaAttribute}) => {
+    const setPausado = (pausado: boolean) => {
+      // Leemos el eid del video en el momento del evento, no antes
+      const {reproductorVideo} = schemaAttribute.get(eid)
       const controles = ecs.VideoControls.get(world, reproductorVideo)
-      
-      if (controles) {
-        ecs.VideoControls.mutate(world, reproductorVideo, (c) => {
-          c.paused = !c.paused
-          console.log("Estado del video pausado:", c.paused)
-        })
-      } else {
-        console.error("No se encontró el reproductor de video en el plano asignado")
+
+      if (!controles) {
+        console.error('No se encontró el reproductor de video en el plano asignado')
+        return
       }
+
+      ecs.VideoControls.mutate(world, reproductorVideo, (c) => {
+        c.paused = pausado
+        return false
+      })
     }
 
-    // Escuchamos los toques (en PC y en móvil)
-    world.events.addListener(component.eid, 'pointerdown', alternarVideo)
-    world.events.addListener(component.eid, 'click', alternarVideo)
-  }
+    ecs.defineState('pausado')
+      .initial()
+      .onEnter(() => {
+        setPausado(true)
+        ecs.Hidden.remove(world, eid) // se muestra el botón "Reproducir"
+      })
+      .onEvent(ecs.input.SCREEN_TOUCH_START, 'reproduciendo')
+
+    ecs.defineState('reproduciendo')
+      .onEnter(() => {
+        setPausado(false)
+        ecs.Hidden.set(world, eid) // se oculta el botón
+      })
+      .onEvent(ecs.input.SCREEN_TOUCH_START, 'pausado')
+  },
 })
